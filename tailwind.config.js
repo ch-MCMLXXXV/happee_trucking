5a578744b26aa8488a8ec50a4e247dc79fc9773f
Courtney Harris <cjharris1985@gmail.com>
2023-01-19 23:26:43 -0600

    first commit; home page partially working

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+   content: [
+      './pages/**/*.{js,ts,jsx,tsx}',
+      './components/**/*.{js,ts,jsx,tsx}',
+      // Or if using `src` directory:
+      './src/**/*.{js,ts,jsx,tsx}',
+   ],
+   theme: {
+      extend: {
+         backgroundPosition: {
+            'bottom-1': 'center bottom 20%',
+         },
+         backgroundSize: {
+            128: '32rem',
+         },
+         backgroundImage: {
+            trucking:
+               "url('https://images.unsplash.com/photo-1473445730015-841f29a9490b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2340&q=80')",
+            trucking2:
+               "url('https://images.pexels.com/photos/93398/pexels-photo-93398.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2')",
+         },
+      },
+   },
+   plugins: [],
+};
